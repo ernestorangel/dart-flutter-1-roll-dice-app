7cc6f0b7caf582cd16fedba5eb4269a18bb89947
Ernesto Rangel <ernestojrcjunior@gmail.com>
2026-06-24 22:15:00 -0300

    feat: Enhance main.dart with a gradient background and structured layout

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,31 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(home: Text('Hello World')));
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                const Color.fromARGB(255, 62, 0, 169),
+                const Color.fromARGB(255, 39, 0, 65),
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: const Center(
+            child: Text(
+              'Hello World',
+              style: TextStyle(
+                color: Color.fromARGB(255, 240, 240, 240),
+                fontSize: 28,
+              ),
+            ),
+          ),
+        ),
+      ),
+    ),
+  );
 }
