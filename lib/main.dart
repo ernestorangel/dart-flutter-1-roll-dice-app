8416edaf08faf1d89d685e13db1b5c766d8bdcdb
Ernesto Rangel <ernestojrcjunior@gmail.com>
2026-06-24 22:30:19 -0300

    feat: Refactor main.dart to use GradientContainer for improved readability and structure

--- lib/main.dart
+++ lib/main.dart
@@ -2,30 +2,37 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
-      home: Scaffold(
-        body: Container(
-          decoration: BoxDecoration(
-            gradient: LinearGradient(
-              colors: [
-                const Color.fromARGB(255, 62, 0, 169),
-                const Color.fromARGB(255, 39, 0, 65),
-              ],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-            ),
-          ),
-          child: const Center(
-            child: Text(
-              'Hello World',
-              style: TextStyle(
-                color: Color.fromARGB(255, 240, 240, 240),
-                fontSize: 28,
-              ),
-            ),
+    const MaterialApp(
+      home: Scaffold(body: GradientContainer()),
+    ),
+  );
+}
+
+class GradientContainer extends StatelessWidget {
+  const GradientContainer({super.key});
+
+  @override
+  Widget build(context) {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            Color.fromARGB(255, 62, 0, 169),
+            Color.fromARGB(255, 39, 0, 65),
+          ],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+      ),
+      child: const Center(
+        child: Text(
+          'Hello World',
+          style: TextStyle(
+            color: Color.fromARGB(255, 240, 240, 240),
+            fontSize: 28,
           ),
         ),
       ),
-    ),
-  );
+    );
+  }
 }
