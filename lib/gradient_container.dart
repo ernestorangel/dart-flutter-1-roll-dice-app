05600b404082ee0a4f7d919e4f14b822535a09ce
Ernesto Rangel <ernestojrcjunior@gmail.com>
2026-06-24 22:40:21 -0300

    feat: Refactor GradientContainer to use variables for alignment and improve readability

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,20 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:roll_dice_app/styled_text.dart';
 
+var startAlignment = Alignment.topLeft;
+var endAligment = Alignment.bottomRight;
+
 class GradientContainer extends StatelessWidget {
   const GradientContainer({super.key});
 
   @override
   Widget build(context) {
     return Container(
-      decoration: const BoxDecoration(
+      decoration: BoxDecoration(
         gradient: LinearGradient(
-          colors: [
+          colors: const [
             Color.fromARGB(255, 62, 0, 169),
             Color.fromARGB(255, 39, 0, 65),
           ],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
+          begin: startAlignment,
+          end: endAligment,
         ),
       ),
       child: const Center(child: StyledText()),
